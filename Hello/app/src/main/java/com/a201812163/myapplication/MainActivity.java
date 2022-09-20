3dd66cd8397e19bc0ce6d8fa6f051aac864a629e
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-09-20 20:15:33 +0900

    add exmple button

--- Hello/app/src/main/java/com/a201812163/myapplication/MainActivity.java
+++ Hello/app/src/main/java/com/a201812163/myapplication/MainActivity.java
@@ -3,22 +3,16 @@ package com.a201812163.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.button);
 
-
-        // 인텐드 사용 사이트 전화
 
     }
 
