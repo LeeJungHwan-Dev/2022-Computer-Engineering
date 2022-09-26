1b5df931ddd8c9ec7fef20f78dc37e4fbafd253b
JungHwan_Lee <aftermoa@gmail.com>
2022-09-26 16:04:51 +0900

    Add : button example

---
+++ Button/app/src/main/java/com/a201812163/button/MainActivity.java
@@ -0,0 +1,60 @@
+package com.a201812163.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button naver , phone , toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     /*  naver = findViewById(R.id.Go_naver);
+        phone = findViewById(R.id.Go_phone);
+        toast = findViewById(R.id.view_Toast);
+
+
+        naver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"));
+                startActivity(intent);
+            }
+        });
+
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1234-5678"));
+                startActivity(intent);
+            }
+        });
+
+
+        toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"버튼 클릭함..!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+      */
+
+    }
+
+
+
+
+
+}
