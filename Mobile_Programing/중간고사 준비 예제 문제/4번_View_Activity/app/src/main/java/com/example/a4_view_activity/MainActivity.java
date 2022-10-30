bd12d7dec5a93c8a1caf19a843fb5fa0f9d5dcfb
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-10-30 20:19:20 +0900

    Add : 연습 문제 주석 추가

--- Mobile_Programing/중간고사 준비 예제 문제/4번_View_Activity/app/src/main/java/com/example/a4_view_activity/MainActivity.java
+++ Mobile_Programing/중간고사 준비 예제 문제/4번_View_Activity/app/src/main/java/com/example/a4_view_activity/MainActivity.java
@@ -13,6 +13,28 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    /********** 문제 **********8
+     *
+     *
+     * 정민이는 가로 방향으로 버튼을 3개 배치하고, 그 버튼 아래에 버튼을 제외하고 화면을 꽉 채우는 View를 배치하여
+     * 버튼을 눌렀을 때 View에 원하는 엑티비티를 보여주는 앱을 만들고 싶어한다.
+     *
+     * 버튼1을 누르면 텍스트 뷰에 화면1을 출력하고
+     * 버튼2를 누르면 바뀐 엑티비티에 파란색 배경을 출력하고
+     * 버튼3을 누르면 화면을 전환하고 스낵바를 출력후 “엑티비티3” 이라는 내용을 출력해라.
+     *
+     * 조건
+     *
+     * 반드시 하나의 엑티비티안에서 작업해야한다.
+     * Intent를 사용하여 인텐트를 전환하면 안된다.
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+
     Button btn1, btn2, btn3;
     LinearLayout view;
 
