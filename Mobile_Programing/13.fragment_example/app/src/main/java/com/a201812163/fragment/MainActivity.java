5a096cd9f02c0bd64debcbd2feb0b08f050831b7
LeeJungHwan-Dev <aftermoa@gmail.com>
2022-11-04 21:11:35 +0900

    Add : fragment example

---
+++ Mobile_Programing/13.fragment_example/app/src/main/java/com/a201812163/fragment/MainActivity.java
@@ -0,0 +1,42 @@
+package com.a201812163.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    First_BlankFragment frm;
+    Second_BlankFragment srm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frm = new First_BlankFragment();
+        srm = new Second_BlankFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.continer,
+                frm).commit();
+
+    }
+
+
+
+    public void onFragmentChange(int i ){
+
+
+        if( i == 0 ){
+            getSupportFragmentManager().beginTransaction().replace(R.id.continer,
+                    srm).commit();
+        }else{
+            getSupportFragmentManager().beginTransaction().replace(R.id.continer,
+                    frm).commit();
+        }
+
+
+    }
+
+
+}
